b829d6f116281d5c35b88caf3cbd866fcda19c48
Michelangelo Giacomelli <michelangelo@air13.local>
2018-03-23 16:47:54 +0100

    First import

---
+++ get_all_cards_db.py
@@ -0,0 +1,97 @@
+import urllib2
+import json
+import os
+import urllib
+
+URL = 'https://api.scryfall.com/cards'
+DATAS = []
+RELATED = []
+RHASH = {}
+
+def get_json(uri):
+    response = urllib2.urlopen(uri)
+    data = json.load(response)
+    print "url is => %s" % uri
+    print "total cards %s" % data['total_cards']
+    print "has more %s" % data['has_more']
+    return {'next': data['next_page'], 'set': data['data']}
+
+def get_data(item, commond_data):
+    print "card => %s" % item['name']
+    text = ''
+    if 'oracle_text' in item:
+        text = item['oracle_text']
+    tmp = {
+        'id': commond_data['id'],
+        'cmc': commond_data['cmc'],
+        'layout': commond_data['layout'],
+        'set': commond_data['set'],
+        'set_name': commond_data['set_name'],
+        'set_name': commond_data['set_name'],
+        'multiverse_ids': commond_data['multiverse_ids'],
+        'name': item['name'],
+        'mana_cost': item['mana_cost'],
+        'colors': item['colors'],
+        'type_line': item['type_line'],
+        'img': item['image_uris']['normal'],
+        'img_small': item['image_uris']['small'],
+        'img_large': item['image_uris']['large'],
+        'text': text,
+    }
+    return tmp
+
+def extrac_data(t_set):
+    for item in t_set:
+        if 'legalities' in item:
+            if item['legalities']['pauper'] == 'legal':
+                commond_data = {
+                    'rarity': item['rarity'],
+                    'id': item['oracle_id'],
+                    'cmc': item['cmc'],
+                    'layout': item['layout'],
+                    'set': item['set'],
+                    'set_name': item['set_name'],
+                    'multiverse_ids': item['multiverse_ids'],
+                }
+                if item['layout'] == 'split':
+                    middle = item
+                    text1 = item['card_faces'][0]['name']
+                    text2 = item['card_faces'][1]['name']
+                    ttype = item['card_faces'][0]['type_line'] + "//" + item['card_faces'][1]['type_line']
+                    if  'oracle_text' in item['card_faces'][0]:
+                        text1 = text1 + ':' + item['card_faces'][0]['oracle_text'] + '\n'
+                    if  'oracle_text' in item['card_faces'][1]:
+                        text2 = text2 + ':' + item['card_faces'][1]['oracle_text'] + '\n'
+                    middle['oracle_text'] = "split card\n" + text1 + text2
+                    middle['type_line'] = ttype
+                    tmp = get_data(middle, commond_data)
+                    DATAS.append(tmp)
+                elif item['layout'] == 'transform':
+                    tmp1 = get_data(item['card_faces'][0], commond_data)
+                    tmp2 = get_data(item['card_faces'][1], commond_data)
+                    DATAS.append(tmp1)
+                    DATAS.append(tmp2)
+                    RELATED.append({
+                        'id': item['oracle_id'],
+                        'face1': tmp1['name'],
+                        'face2': tmp2['name']
+                    })
+                    RHASH[item['oracle_id']] = {
+                        'face1': tmp1['name'],
+                        'face2': tmp2['name']
+                    }
+                else:
+                    tmp = get_data(item, commond_data)
+                    DATAS.append(tmp)
+def main(uri):
+    tmp = get_json(uri)
+    extrac_data(tmp['set'])
+    if tmp['next'] is not None:
+        main(tmp['next'])
+    with open('cards.json', 'w') as card_file:
+        json.dump(DATAS, card_file)
+    with open('related.json', 'w') as related_file:
+        json.dump(RELATED, related_file)
+    with open('rhash.json', 'w') as rhash_file:
+        json.dump(RHASH, rhash_file)
+main(URL)
---
+++ get_imagees.py
@@ -0,0 +1,77 @@
+import urllib2
+import json
+import os
+import urllib
+
+URL = 'https://api.scryfall.com/cards?page=51'
+#URL = 'https://api.scryfall.com/cards?page=226'
+
+def get_json(uri):
+    response = urllib2.urlopen(uri)
+    data = json.load(response)
+    print "url is => %s" % uri
+    print "total cards %s" % data['total_cards']
+    print "has more %s" % data['has_more']
+    #if data['has_more'] == False:
+        #import pdb; pdb.set_trace()
+    return {'next': data['next_page'], 'set': data['data']}
+
+def download_image(url, file_name):
+    urllib.urlretrieve(url, file_name)
+
+def make_dir(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+def parse_name(tmp_string):
+    tmp = tmp_string
+    tmp = tmp.strip()
+    tmp = tmp.replace(' ', '_')
+    tmp = tmp.replace('/', "@")
+    tmp = tmp.lower()
+    return tmp
+
+def download(item, number, total):
+    tset = parse_name(item['set'])
+    make_dir('./img/' + tset)
+    fname = './img/' + tset + '/' + parse_name(item['name']) + '.jpg'
+    url = item['url']
+    print "downloading %s, %s of %s" % (url, str(number), str(total))
+    download_image(url, fname)
+
+def extrac_image(t_set):
+    t_all = []
+    for item in t_set:
+        if item['layout'] != 'transform':
+        #if 'image_uris' in item:
+            tmp = {
+                'set': item['set'],
+                'url': item['image_uris']['large'],
+                'name': item['name'],
+            }
+            t_all.append(tmp)
+        if item['layout'] == 'transform':
+            if 'card_faces' in item:
+                for card in item['card_faces']:
+                    if 'image_uris' in card:
+                        tmp = {
+                            'set': item['set'],
+                            'url': card['image_uris']['large'],
+                            'name': card['name'],
+                        }
+                        t_all.append(tmp)
+                    else:
+                        print "unable to extract image from %s, oracle id %s" % (card['name'], item['oracle_id'])
+    total = len(t_all)
+    n = 0
+    for item in t_all:
+        n = n + 1
+        download(item, n, total)
+
+def main(uri):
+    tmp = get_json(uri)
+    extrac_image(tmp['set'])
+    if tmp['next'] is not None:
+        main(tmp['next'])
+
+main(URL)
